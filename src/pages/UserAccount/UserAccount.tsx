e720916b66fe72e36c37a32809154ba1d1aae39a
Oleg Tkach <KratosHome101@gmail.com>
2022-05-13 15:06:00 +0300

    for netlifay

--- src/pages/UserAccount/UserAccount.tsx
+++ src/pages/UserAccount/UserAccount.tsx
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import  {useContext} from "react";
 
 import {PersonalAccount} from "../PersonalAccount/PersonalAccount";
 import "./UserAccount.css";
@@ -7,7 +7,7 @@ import {UserAccountLogin} from "../UserAccountLogin/UserAccountLogin";
 
 export const UserAccount = () => {
     const {isAuth} = useContext(LoginContext);
-    console.log(LoginContext)
+
     return (
         <>
             {
