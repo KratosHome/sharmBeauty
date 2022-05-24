29724b0ed8d4000f34273f1737f810e995065032
Oleg Tkach <KratosHome101@gmail.com>
2022-05-24 15:20:42 +0300

    card page

--- src/pages/CardPage/ProductInCard/ProductInCard.tsx
+++ src/pages/CardPage/ProductInCard/ProductInCard.tsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import "./ProductInCard.css"
 import {useTypeSelector} from "../../../hooks/useTupeSelecrot";
-import {productTypes} from "../../../types/productTypes";
+import {prodTypes, productTypes} from "../../../types/productTypes";
+import {useEffect, useState} from "react";
 
 export const ProductInCard = () => {
-    const {product} = useTypeSelector(
+    const {product, addToCard} = useTypeSelector(
         (state) => state.product);
 
-    console.log(product)
+
+    let bla = product.map((item: any) => item.prod.filter((item: any) => item.id === addToCard.join(" ")))
+    console.log(bla)
+
+
     return (
         <>
             {product.map((item: productTypes) =>
-                <div key={item.name}>
-                    {item.name}
+                <div key={Math.random()} className="ProductInCardContainer">
+                    <img className="ProductInCardImg" src={item.img} alt={item.name}/>
+                    <div>{item.name}</div>
+                    <div>цена</div>
+                    <div>кількість</div>
+                    <div>сумма</div>
                 </div>
             )}
         </>
